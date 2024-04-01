142ebff8d69c552fcb59aee1007ce8e705947c0d
timoniche <dmitriidulaev@gmail.com>
2024-04-01 20:33:04 +0300

    hw4: added test dataset loader

--- hw4/data_preparer.py
+++ hw4/data_preparer.py
@@ -23,6 +23,24 @@ def trainval_shuffled_data(from_file=True):
     return data
 
 
+def test_shuffled_data(from_file=True):
+    datafile = 'test.pkl'
+
+    if from_file:
+        with open(datafile, 'rb') as f:
+            data = pickle.load(f)
+        return data
+
+    good, bad = load_test_samples()
+    data = good + bad
+    shuffle(data)
+
+    with open(datafile, 'wb') as f:
+        pickle.dump(data, f)
+
+    return data
+
+
 def trainval_examples():
     data = trainval_shuffled_data(from_file=True)
 
@@ -35,17 +53,39 @@ def trainval_examples():
     return trainval
 
 
+def load_test_samples():
+    _, _, test = _load_sberquad_dataset()
+
+    questions = test['question'].tolist()
+    contexts = test['context'].tolist()
+
+    return _inflate_with_negative_samples(
+        questions=questions,
+        contexts=contexts,
+    )
+
+
 def load_trainval_samples():
     train, val, _ = _load_sberquad_dataset()
 
     questions = train['question'].tolist() + val['question'].tolist()
     contexts = train['context'].tolist() + val['context'].tolist()
 
+    return _inflate_with_negative_samples(
+        questions=questions,
+        contexts=contexts,
+    )
+
+
+def _inflate_with_negative_samples(
+        questions,
+        contexts,
+        delta=30,  # in sberquad dataset same contexts are close
+):
     good_q_c_cos = list(zip(questions, contexts, [1.0] * len(questions)))
 
     bad_q_c_cos = []
     n = len(good_q_c_cos)
-    delta = 30  # in sberquad dataset same contexts are close
     for i in range(n):
         cur_q, cur_c, _ = good_q_c_cos[i]
         next_q, next_c, _ = good_q_c_cos[(i + delta) % n]
@@ -78,7 +118,8 @@ def _load_sberquad_dataset():
 
 
 def main():
-    trainval_examples()
+    data = test_shuffled_data(from_file=True)
+    print(len(data))
 
 
 if __name__ == '__main__':
